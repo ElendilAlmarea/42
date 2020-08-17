8aae602a94d9d59498dc8102300b76482951e469
yvanat <yvanat@student.42.fr>
2020-08-17 15:54:40 +0200

    not finished

--- ft_services/setup.sh
+++ ft_services/setup.sh
@@ -16,12 +16,18 @@ then
 	minikube addons enable dashboard
 fi
 
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 MINIKUBE_IP=$(minikube ip)
 
 #cp srcs/wordpress/wpconfig.sql srcs/wordpress/wpconfig-target.sql
 #sed -i '' "s/##MINIKUBE_IP##/$MINIKUBE_IP/g" srcs/wordpress/wpconfig-target.sql
 #cp srcs/ftps/entrypoint srcs/ftps/entrypoint-target
 #sed -i '' "s/##MINIKUBE_IP##/$MINIKUBE_IP/g" srcs/ftps/entrypoint-target
+#cp srcs/metallb.yaml srcs/metallb-target.yaml
+#sed -i '' "s/##MINIKUBE_IP##/$MINIKUBE_IP/g" srcs/metallb.yaml
 
 eval $(minikube docker-env)
 docker build -t nginx_alpine srcs/nginx
